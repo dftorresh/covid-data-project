8e1530f5da1b2c780bb1eabc67cfef39cf43e9ae
dftorres <dafetohe2@gmail.com>
2023-03-08 17:46:53 -0500

    Databricks notebook to process Covid Hospital admission data created

---
+++ databricks/includes/configuration.py
@@ -0,0 +1,6 @@
+# Databricks notebook source
+raw_data_folder_path = 'dbfs:/mnt/raw'
+processed_data_folder_path = 'dbfs:/mnt/processed'
+lookup_data_folder_path = 'dbfs:/mnt/lookup'
+
+# COMMAND ----------
